6d2f7386e0474c8276530f282173482af77e6356
Juan Delgado <70461429+V3rax0@users.noreply.github.com>
2022-11-20 16:04:25 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 let inputEmail = document.getElementById("email");
 const buttonEnviar = document.getElementById("newsletter__button");
 
-buttonEnviar.addEventListener("click", () => {
+
+/*
+buttonEnviar.addEventListener("click", (event) => {
+    event.preventDefault();
     sendEmail(inputEmail.value);
 });
 
@@ -12,29 +15,27 @@ function sendEmail(email) {
 
     if (expReg.test(email) == true) {
         inputEmail.style.border = "1px solid black";
-        //EmailJS
-        document.getElementById('newsletter__form')
-            .addEventListener('submit', function (event) {
-                event.preventDefault();
-
-                buttonEnviar.value = 'Enviando...';
-
-                const serviceID = 'default_service';
-                const templateID = 'template_65ao7yk';
-
-                emailjs.sendForm(serviceID, templateID, this)
-                    .then(() => {
-                        buttonEnviar.value = 'Enviar';
-                        console.log('Sent!');
-                    }, (err) => {
-                        buttonEnviar.value = 'Enviar';
-                        alert(JSON.stringify(err));
-                    });
-            });
-
-        buttonEnviar.value = '';
 
     } else {
         inputEmail.style.border = "1px solid red";
     }
 }
+*/
+
+//EmailJS
+document.getElementById('newsletter__form')
+    .addEventListener('submit', function(event) {
+        event.preventDefault();
+       
+        const serviceID = 'default_service';
+        const templateID = 'template_65ao7yk';     
+       
+          emailjs.sendForm(serviceID, templateID, this)
+           .then(() => {
+            buttonEnviar.value = 'Enviar';
+             console.log('Sent!');
+           }, (err) => {
+            buttonEnviar.value = 'Enviar';
+             alert(JSON.stringify(err));
+           });
+});
