2d46073a074a11146a5cad12f90e966cab3f8184
V3rax0 <juangeek369@gmail.com>
2022-11-20 16:32:13 -0500

    Formulario Newsletter funcionando

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
-//validación del correo
-let inputEmail = document.getElementById("email");
 const buttonEnviar = document.getElementById("newsletter__button");
-
-
-/*
-buttonEnviar.addEventListener("click", (event) => {
-    event.preventDefault();
-    sendEmail(inputEmail.value);
-});
-
-function sendEmail(email) {
-    let expReg =
-        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
-
-    if (expReg.test(email) == true) {
-        inputEmail.style.border = "1px solid black";
-
-    } else {
-        inputEmail.style.border = "1px solid red";
-    }
-}
-*/
-
 //EmailJS
 document.getElementById('newsletter__form')
     .addEventListener('submit', function(event) {
@@ -33,7 +10,9 @@ document.getElementById('newsletter__form')
           emailjs.sendForm(serviceID, templateID, this)
            .then(() => {
             buttonEnviar.value = 'Enviar';
-             console.log('Sent!');
+            console.log('Enviado!')
+            inputEmail.value = '';
+
            }, (err) => {
             buttonEnviar.value = 'Enviar';
              alert(JSON.stringify(err));
